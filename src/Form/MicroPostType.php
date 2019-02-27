988ac53d6e97b82648b0e77e12adb547b4f59e2b
Jaime Seuma <jaims.seuma@gmail.com>
2019-02-27 16:34:44 +0100

    Lecture 30 form view and submission

---
+++ src/Form/MicroPostType.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaims
+ * Date: 27/02/19
+ * Time: 15:12
+ */
+
+namespace App\Form;
+
+
+use App\Entity\MicroPost;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MicroPostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('text', TextareaType::class)
+            ->add('save', SubmitType::class);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults(
+            [
+                'data_class' => MicroPost::class
+            ]
+        );
+    }
+
+}
