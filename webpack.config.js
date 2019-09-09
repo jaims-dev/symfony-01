4b1b6d2a9de1de399e7851b2cd01fd6d63ea2295
Jaime Seuma <jaims.dev@gmail.com>
2019-09-09 20:26:49 +0200

    Up to lecture 46

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,27 @@
 var Encore = require('@symfony/webpack-encore');
 
 Encore
-    // directory where compiled assets will be stored
+    // the project directory where compiled assets will be stored
     .setOutputPath('public/build/')
-    // public path used by the web server to access the output path
+    // the public path used by the web server to access the previous directory
     .setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+    .cleanupOutputBeforeBuild()
+    .enableSourceMaps(!Encore.isProduction())
+    // uncomment to create hashed filenames (e.g. app.abc123.css)
+    // .enableVersioning(Encore.isProduction())
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Add 1 entry for each "page" of your app
-     * (including one that's included on every page - e.g. "app")
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if you JavaScript imports CSS.
-     */
+    // uncomment to define the assets of the project
     .addEntry('js/app', [
-        './node_modules/jquery/dist/jquery.slim.min.js',
-        './node_modules/popper.js/dist/popper.min.js',
+        './node_modules/jquery/dist/jquery.slim.js',
+        './node_modules/popper.js/dist/umd/popper.min.js',
         './node_modules/bootstrap/dist/js/bootstrap.min.js',
-        './node_modules/holderjs/holder.min.js',
-        './assets/js/app.js'
+        './node_modules/holderjs/holder.min.js'
     ])
     .addStyleEntry('css/app', [
         './node_modules/bootstrap/dist/css/bootstrap.min.css',
-        './assets/css/app.css']
-        )
+        './assets/css/app.css'
+    ])
+    
     //.addEntry('page1', './assets/js/page1.js')
     //.addEntry('page2', './assets/js/page2.js')
 
@@ -36,7 +30,7 @@ Encore
 
     // will require an extra script tag for runtime.js
     // but, you probably want this, unless you're building a single-page app
-    .enableSingleRuntimeChunk()
+    //.enableSingleRuntimeChunk()
 
     /*
      * FEATURE CONFIG
