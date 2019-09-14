7d3624f9715bc3e3548b9f2f0672dcb1912eff3e
Jaime Seuma <jaims.dev@gmail.com>
2019-09-14 10:58:17 +0200

    Lecture 55, Doctrine Lyfecicle calls

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -108,7 +108,7 @@ class MicroPostController
     {
         $user = $tokenStorage->getToken()->getUser();
         $microPost = new MicroPost();
-        $microPost->setTime(new \DateTime());
+//        $microPost->setTime(new \DateTime());
         $microPost->setUser($user);
         $form = $this->formFactory->create(
             MicroPostType::class,
